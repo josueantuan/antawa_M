c334867a211462e8502e68f99f877cf39ea2889d
josueantuan <antuan_1996@hotmail.com>
2019-01-04 18:24:13 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Perfil', component: HomePage },
+      { title: 'Monitor', component: ListPage },
+      { title: 'Soporte', component: ListPage }
     ];
 
   }
